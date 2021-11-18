8214f7cb47b2b3c07657452de5166ec5425a5fe9
Zachary Fechko <zachary.fechko@wsu.edu>
2021-11-18 15:35:59 -0800

    Finished BST Class

--- PA 8 Data Analysis Using BSTs/BST.cpp
+++ PA 8 Data Analysis Using BSTs/BST.cpp
@@ -1,1 +1,80 @@
 #include "BST.h"
+
+
+BST::BST()
+{
+	pRoot = nullptr;
+}
+
+BST::~BST()
+{
+	chopTree(pRoot);
+}
+
+void BST::chopTree(Node* pTree)
+{
+	if(pTree != nullptr)
+	{
+		chopTree(pTree->getLeft());
+		chopTree(pTree->getRight());
+		delete pTree;
+	}
+}
+
+void BST::insert(string newData, int newUnits)
+{
+	TransactionNode* insertionNode = new TransactionNode(newData, newUnits);
+	insert(dynamic_cast<TransactionNode*>(pRoot), insertionNode);
+}
+
+void BST::insert(TransactionNode* pTree, TransactionNode* newNode)
+{
+	if (pRoot == nullptr)
+	{
+		pRoot = newNode;
+	}
+	else
+	{
+		if (newNode->getUnits() < pTree->getUnits()) //if we need to go left
+		{
+			if (pTree->getLeft() == nullptr)
+			{
+				pTree->setLeft(newNode);
+			}
+			else
+			{
+				insert(dynamic_cast<TransactionNode*>(pTree->getLeft()), newNode);
+			}
+		}
+		else if (newNode->getUnits() > pTree->getUnits()) // if we need to go to the right
+		{
+			if (pTree->getRight() == nullptr)
+			{
+				pTree->setRight(newNode);
+			}
+			else
+			{
+				insert(dynamic_cast<TransactionNode*>(pTree->getRight()), newNode);
+			}
+		}
+		else
+		{
+			cout << "OOPS, THERE'S A DUPLICATE VALUE BEING ENTERED, THAT'S A NONO" << endl;
+		}
+	}
+}
+
+Node* BST::getRoot()
+{
+	return pRoot;
+}
+
+void BST::inOrderTraversal(Node* pTree)
+{
+	if (pTree != nullptr)
+	{
+		inOrderTraversal(pTree->getLeft());
+		pTree->printData();
+		inOrderTraversal(pTree->getRight());
+	}
+}
--- PA 8 Data Analysis Using BSTs/BST.h
+++ PA 8 Data Analysis Using BSTs/BST.h
@@ -14,6 +14,6 @@ public:
 	~BST();
 
 	Node* getRoot();
-
+	void insert(string newData, int newUnits);
 };
 
